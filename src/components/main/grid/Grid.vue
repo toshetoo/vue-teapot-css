76670833b6bd82bee8111b4dd014c944a1e39723
Todor Ivanov <todor.ivanov@primeholding.com>
2020-01-18 13:03:40 +0200

    Add 12 column Grid

--- src/components/main/grid/Grid.vue
+++ src/components/main/grid/Grid.vue
@@ -33,7 +33,41 @@
                     </CardBody>
                 </CardView>
             </div>
-        </div>        
+        </div>  
+        <div class="row">
+            <div class="col pl-3 pr-3">
+                <CardView>
+                    <CardHeader>                        
+                        <p>12 Column Grid</p>
+                    </CardHeader>
+                    <CardBody>
+                        <div class="row">
+                            <div class="col">
+                                <small>The framework provides column classes for each device.</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <HtmlPreview>
+                               <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-x-lg-3" style="background-color: red">
+                                        COL 1
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-x-lg-3" style="background-color: blue">
+                                        COL 2
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-x-lg-3" style="background-color: red">
+                                        COL 3
+                                    </div>
+                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-x-lg-3" style="background-color: blue">
+                                        COL 4
+                                    </div>
+                                </div>
+                            </HtmlPreview>
+                        </div>
+                    </CardBody>
+                </CardView>
+            </div>
+        </div>      
     </div>
 </template>
 
